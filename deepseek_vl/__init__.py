717ed7639c3ab3f3e7726a7e50b81d5bf2172a8a
Nicola Dall'Asen <nicola.dallasen@gmail.com>
2024-03-13 15:34:10 +0100

    feat: automatically patch `collections` for >python 3.10 support (#21)
    Co-authored-by: Bo Liu <benjaminliu.eecs@gmail.com>

--- deepseek_vl/__init__.py
+++ deepseek_vl/__init__.py
@@ -16,3 +16,16 @@
 # COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
 # IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 # CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+
+# check if python version is above 3.10
+import sys
+
+if sys.version_info >= (3, 10):
+    print("Python version is above 3.10, patching the collections module.")
+    # Monkey patch collections
+    import collections
+    import collections.abc
+
+    for type_name in collections.abc.__all__:
+        setattr(collections, type_name, getattr(collections.abc, type_name))
